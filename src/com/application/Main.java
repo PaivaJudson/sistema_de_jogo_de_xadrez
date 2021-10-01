4f5b3c681e7fa0a978d8ab3110df98f3b07db49d
Judson Paiva <judson.paiva@hotmail.com>
2021-10-01 22:03:32 +0100

    criado as classes Board e Piece

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,14 +1,14 @@
 package com.application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Position position = new Position(3, 5);
-
-        System.out.println(position.toString());
+        Board board = new Board(5, 5);
+        System.out.println(board.getColumns());
 
     }
 }
