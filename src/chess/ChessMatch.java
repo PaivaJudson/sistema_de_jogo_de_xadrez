023f79d48d5088a27b0b12f1b4b190b9f7916a59
Judson Paiva <judson.paiva@hotmail.com>
2021-10-03 23:30:06 +0100

    movimentos possíveis para a peça...

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,6 +46,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)){
             throw new ChessException("Não existe peça na posição de origem");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Não existe movimentação possível para a peça");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
